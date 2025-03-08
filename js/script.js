fcaab7c9a7ab4ecb54753eb7be94392f68ab6ede
Qurbonoff <jonibekqurbonov570@gmail.com>
2025-03-08 14:04:55 +0300

    js + loop

--- js/script.js
+++ js/script.js
@@ -36,6 +36,7 @@ anime({
         {value: .1, easing: 'easeOutSine', duration: 500},
         {value: 1, easing: 'easeInOutQuad', duration: 1200}
     ],
+    loop: true,
     delay: anime.stagger(200, {grid: [14, 5], from: 'center'})
 });
 
